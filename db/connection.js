23d7d1ed7e06bf93c7ff5d80d96d9b830ebec06a
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2022-03-08 16:28:52 -0700

    connections

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_KEY,
+    database: "employees"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
